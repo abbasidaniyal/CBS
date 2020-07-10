a72d34b9a1c3de6fbb56b9fb2248b58f578562db
Daniyal <abbasi.daniyal98@gmail.com>
2020-07-10 21:17:05 +0530

    add: linting and sudo for git

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 ssh -o "StrictHostKeyChecking=no" -i /tmp/deploy.cer  -A ${HOST_USER}@${HOST_IP} << EOT
 cd /home/website/CBS
-git stash
-git pull origin master
+sudo git stash
+sudo git pull origin master
 source env/bin/activate
 pip install -r requirements.txt
 python manage.py collectstatic  --noinput
