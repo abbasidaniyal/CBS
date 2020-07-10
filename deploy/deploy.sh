e5e7eb86d8e206c647ecd31cd2517b8fe6a71676
Daniyal <abbasi.daniyal98@gmail.com>
2020-07-10 14:04:37 +0530

    cd pipeline setup

---
+++ deploy/deploy.sh
@@ -0,0 +1,10 @@
+ssh -o "StrictHostKeyChecking=no" -i /tmp/deploy_rsa -A ${HOST_USER}@${HOST_IP} << EOT
+cd /home/website/CBS
+git pull origin master
+source env/bin/activate
+pip install -r requirements.txt
+python manage.py collectstatic  --noinput
+python manage.py migrate  --noinput
+sudo service website restart
+echo "Deploy Finished"
+EOT
