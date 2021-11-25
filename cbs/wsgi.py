8a1673d5da4239f55e06be1b555dc40d4b570945
Daniyal Abbasi <daniyal.abbasi@innovaccer.com>
2021-11-25 13:58:02 +0530

    Removed DOTENV from wsgi.py

--- cbs/wsgi.py
+++ cbs/wsgi.py
@@ -11,10 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dotenv import load_dotenv, find_dotenv
-
-
-load_dotenv(find_dotenv())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cbs.settings")
 
